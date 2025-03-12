a06a5af1790bbd305c482e0719449d5b2b2eeed0
SUNDAR <aaransoftware@gmail.com>
2025-03-12 09:40:53 +0530

    #29 fixing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Aaran\Auth\Identity\Database\Seeders\S000_UserSeeder;
-use Aaran\Auth\Identity\Database\Seeders\S001_RoleSeeder;
 use Aaran\Common\Database\Seeders\S000_CommonSeeder;
 use Aaran\Core\Database\Seeders\S00_CoreSeeder;
 use Aaran\Master\Database\Seeders\S199_MasterSeeder;
@@ -11,14 +10,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         S00_CoreSeeder::run();
         S000_UserSeeder::run();
-        S001_RoleSeeder::run();
         S000_CommonSeeder::run();
         S199_MasterSeeder::run();
     }
