9087a6f3bae18797eda33dc18fbc1002553c7cbe
SUNDAR <aaransoftware@gmail.com>
2025-03-13 23:08:13 +0530

    #31 fixing something

--- aaran/Assets/Resources/components/button/save-x.blade.php
+++ aaran/Assets/Resources/components/button/save-x.blade.php
@@ -1,5 +1,5 @@
 <button type="submit"
-    class="tab-button sm:px-6 px-4 sm:py-[7px] py-[6px] relative rounded group overflow-hidden font-medium
+    class="tab-button sm:px-6 px-4 sm:py-[7px] py-[6px] relative rounded group overflow-hidden font-medium cursor-pointer
     bg-green-600 inline-block text-center"
 {{$attributes}}>
                 <span
@@ -10,7 +10,7 @@
                Save
             </span>
     <span
-        class="relative hidden group-hover:block group-hover:text-white sm:px-[7.5px] px-[3.5px] sm:py-[2px] py-[0]">
+        class="relative hidden group-hover:block group-hover:text-white sm:px-[6.5px] px-[4.5px] sm:py-[2px] py-[0]">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                   <path fill-rule="evenodd"
                         d="M9 1.5H5.625c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5Zm6.61 10.936a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 14.47a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
